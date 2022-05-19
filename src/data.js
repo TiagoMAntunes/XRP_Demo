c4a754571ed64187d7fca228cb6b338cb4df32c2
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2022-05-19 12:27:25 +0100

    working shop

---
+++ src/data.js
@@ -0,0 +1,97 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Milka Chocolate",
+    img: "img/product-1.png",
+    price: 5,
+    info:
+      "A delicious milka chocolate for fun with your family",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Potatoes (1kg)",
+    img: "img/product-2.png",
+    price: 7,
+    info:
+      "Delicious potatoes for steaming, frying and baking",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Cabbage (1kg)",
+    img: "img/product-3.png",
+    price: 4,
+    info:
+      "Tasty and beautifully looking cabbage for cooking",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Onion (1kg)",
+    img: "img/product-4.png",
+    price: 8,
+    info:
+      "They might make you cry, but it's of happiness",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Cauliflower (1kg)",
+    img: "img/product-5.png",
+    price: 11,
+    info:
+      "No one at this store likes this but you will be happy with it",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Pepper (1kg)",
+    img: "img/product-6.png",
+    price: 13,
+    info:
+      "Tasty pepper for your salads! It's a good source of vitamin C",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Broccoli (1kg)",
+    img: "img/product-7.png",
+    price: 6.5,
+    info:
+      "Tasty and vitamin C rich broccoli for cooking",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Carrots (1kg)",
+    img: "img/product-8.png",
+    price: 8.7,
+    info:
+      "Your eyes will be prettier than Bugs Bunny's with these carrots",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = storeProducts[0];
